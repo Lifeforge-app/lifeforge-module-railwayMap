f582be38712e02a5ca0681eba3bf6fd883493866
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 11:59:39 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,11 +2,8 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Railway Map',
-  icon: 'uil:subway',
   provider: lazy(() => import('@/providers/RailwayMapProvider')),
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Information'
 } satisfies ModuleConfig
